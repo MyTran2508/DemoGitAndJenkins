b909c2ec8e47d72b38398fb20fe26073ba6076ca
Trần Chí Mỹ <102143951+MyTran2508@users.noreply.github.com>
2023-03-04 21:07:20 +0700

    demojenkins

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!!!! Test ok con dê";
+        return "Hello, World!!!! Hoàn Tất Demo Jenkins";
     }
 }
